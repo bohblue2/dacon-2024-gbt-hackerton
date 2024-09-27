4b0cc3b6eecebd7a967b7c82a67240aeee13a238
BrianBae <brian.bae.contact@gmail.com>
2024-09-27 11:16:47 +0900

    fix: correct typeo

--- startup.sh
+++ startup.sh
@@ -1,19 +1,4 @@
-git clone https://github.com/bohblue2/dacon-2024-gbt-hackerton
-
-sudo apt-get install unzip -y
 if [ -f /etc/zsh/zshrc ]; then
-    echo "ZSH detected"
-    conda init zsh && source ~/.zshrc
-elif [ -f ~/.bashrc ]; then
-    echo "Bash detected"
-    conda init bash && source ~/.bashrc
-else
-    echo "Neither ZSH nor Bash detected, defaulting to Bash"
-    conda init bash && source ~/.bashrc
-fi
-
-if command -v tmux &> /dev/null; then
-    echo "tmux is installedif [ -f /etc/zsh/zshrc ]; then
     echo "ZSH detected"
     conda init zsh && source ~/.zshrc
 elif [ -f ~/.bashrc ]; then
@@ -30,22 +15,6 @@ sudo apt-get install unzip -y
 conda activate base
 conda install -c huggingface transformers huggingface_hub scikit-learn -y
 pip install uv && uv pip install wandb pgwalker
-# NOTE: wandb login with your api key 
-
-# Dacon
-wget https://bit.ly/3gMPScE -O dacon_submit_api-0.0.4-py3-none-any.zip
-unzip dacon_submit_api-0.0.4-py3-none-any.whl 
-pip install dacon_submit_api-0.0.4-py3-none-any.whl
-rm -rf dacon_submit_api-0.0.4-py3-none-any.whl "
-else
-    echo "tmux is not installed"
-fi
-conda activate base
-conda install -c huggingface transformers huggingface_hub scikit-learn -y
-pip install uv
-
-# waandb 
-uv pip install wandb pgwalker
 # NOTE: wandb login with your api key 
 
 # Dacon
